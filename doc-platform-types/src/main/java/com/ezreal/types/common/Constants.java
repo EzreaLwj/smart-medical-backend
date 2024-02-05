47168276985ef030c99f485cc00e2c83195415bb
ezreal <956371950@qq.com>
2024-02-05 19:33:36 +0800

    feat: 添加返回值类型

--- doc-platform-types/src/main/java/com/ezreal/types/common/Constants.java
+++ doc-platform-types/src/main/java/com/ezreal/types/common/Constants.java
@@ -1,6 +1,5 @@
 package com.ezreal.types.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
@@ -12,7 +11,11 @@ public class Constants {
     @Getter
     public enum ResponseCode {
         SUCCESS("0000", "成功"),
-        UN_ERROR("0001", "未知失败");
+        UN_ERROR("0001", "未知失败"),
+        LOGIN_FAIL("0002", "账号或者密码错误"),
+        UN_LOGIN("0003", "用户未登录"),
+        ERROR_PARAMS("0004", "参数错误"),
+        USER_EXIST("0005", "用户已经存在");
 
         private final String code;
         private final String info;
