bb81db8cdf62868d523b11e1fe39460386cf9489
ezreal <956371950@qq.com>
2024-02-05 19:44:23 +0800

    feat: 添加建表语句

---
+++ doc-platform-app/sql/base.sql
@@ -0,0 +1,19 @@
+create table doc_platform_user
+(
+    id          bigint unsigned auto_increment comment '自增主键'
+        primary key,
+    user_id     varchar(32)  default '0'               not null comment '用户id',
+    nick_name   varchar(32)  default ''                not null comment '用户昵称',
+    description varchar(128) default ''                not null comment '个人介绍',
+    email       varchar(32)  default ''                not null comment '个人邮箱',
+    account     varchar(32)  default ''                not null comment '用户账号',
+    password    varchar(32)  default ''                not null comment '用户密码',
+    phone       varchar(32)  default ''                not null comment '手机号码',
+    avatar      varchar(128) default ''                not null comment '用户头像',
+    location    varchar(512) default ''                not null comment '用户住址',
+    create_time timestamp    default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time timestamp    default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    constraint idx_email_password
+        unique (email, password)
+)
+    comment '用户信息表';
