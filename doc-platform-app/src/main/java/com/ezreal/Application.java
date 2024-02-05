6fc6035f66a8b6a5a536ef22d49bc17da36efd2f
ezreal <956371950@qq.com>
2024-02-05 11:48:58 +0800

    引入打包配置

--- doc-platform-app/src/main/java/com/ezreal/Application.java
+++ doc-platform-app/src/main/java/com/ezreal/Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2024/2/2
  */
 @SpringBootApplication
-public class DocPlatformApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(DocPlatformApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
