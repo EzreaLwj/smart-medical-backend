2c60178cd8d0d97639833d58a57a040258928c76
ezreal <956371950@qq.com>
2024-04-16 21:39:47 +0800

    feat: 引入加密算法

---
+++ smart-medical-types/src/main/java/com/ezreal/types/utils/EncryptionUtils.java
@@ -0,0 +1,132 @@
+package com.ezreal.types.utils;
+
+import cn.hutool.core.codec.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.DHParameterSpec;
+import java.security.*;
+
+/**
+ * @author Ezreal
+ * @Date 2024/4/16
+ */
+public class EncryptionUtils {
+
+    private static PublicKey elGamalPublicKey;
+
+    private static PrivateKey elGamalPrivateKey;
+
+    private static final String keySalt = "123";
+
+    private static Cipher cipher;
+
+
+    /**
+     * 初始化公钥、私钥和加密对象
+     */
+    private static void init() {
+        try {
+            if (elGamalPrivateKey == null || elGamalPublicKey == null || cipher == null) {
+                // 公钥加密,私钥解密
+                Security.addProvider(new BouncyCastleProvider());
+
+                // 初始化密钥
+                AlgorithmParameterGenerator algorithmParameterGenerator = AlgorithmParameterGenerator.getInstance("ElGamal");
+                algorithmParameterGenerator.init(256);
+                AlgorithmParameters algorithmParameters = algorithmParameterGenerator.generateParameters();
+
+                DHParameterSpec dhParameterSpec = (DHParameterSpec) algorithmParameters.getParameterSpec(DHParameterSpec.class);
+                KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ElGamal");
+                keyPairGenerator.initialize(dhParameterSpec, new SecureRandom());
+                KeyPair keyPair = keyPairGenerator.generateKeyPair();
+                elGamalPublicKey = keyPair.getPublic();
+                elGamalPrivateKey = keyPair.getPrivate();
+
+                // 创建Cipher对象，用于加密和解密
+                cipher = Cipher.getInstance("ElGamal");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 加密
+     *
+     * @param content 加密的内容
+     * @return 返回Base64处理加密字符串
+     */
+    private static String encrypt(String content) {
+        try {
+            init();
+            // 加密数据
+            cipher.init(Cipher.ENCRYPT_MODE, elGamalPublicKey);
+            String originalMessage = "lwj";
+            byte[] encryptedMessage = cipher.doFinal(originalMessage.getBytes());
+            return Base64.encode(encryptedMessage);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 解密
+     *
+     * @param encryptedMessage 解密二进制字节流
+     * @return 明文
+     */
+    public static String decrypt(byte[] encryptedMessage) {
+        try {
+            init();
+            // 解密数据
+            cipher.init(Cipher.DECRYPT_MODE, elGamalPrivateKey);
+            byte[] decryptedMessage = cipher.doFinal(encryptedMessage);
+            return new String(decryptedMessage);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 测试代码
+     *
+     * @param src
+     */
+    public static void bcElGamal(String src) {
+        try {
+            // 公钥加密,私钥解密
+            Security.addProvider(new BouncyCastleProvider());
+
+            // 1.初始化密钥
+            AlgorithmParameterGenerator algorithmParameterGenerator = AlgorithmParameterGenerator.getInstance("ElGamal");
+            algorithmParameterGenerator.init(256);
+            AlgorithmParameters algorithmParameters = algorithmParameterGenerator.generateParameters();
+
+            DHParameterSpec dhParameterSpec = (DHParameterSpec) algorithmParameters.getParameterSpec(DHParameterSpec.class);
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ElGamal");
+            keyPairGenerator.initialize(dhParameterSpec, new SecureRandom());
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            elGamalPublicKey = keyPair.getPublic();
+            elGamalPrivateKey = keyPair.getPrivate();
+
+            // 2. 创建Cipher对象，用于加密和解密
+            Cipher cipher = Cipher.getInstance("ElGamal");
+
+            // 3. 加密数据
+            cipher.init(Cipher.ENCRYPT_MODE, elGamalPublicKey);
+            String originalMessage = "lwj";
+            byte[] encryptedMessage = cipher.doFinal(originalMessage.getBytes());
+            System.out.println("Encrypted Message:" + Base64.encode(encryptedMessage));
+
+            // 4. 解密数据
+            cipher.init(Cipher.DECRYPT_MODE, elGamalPrivateKey);
+            byte[] decryptedMessage = cipher.doFinal(encryptedMessage);
+            System.out.println("Decrypted Message:" + new String(decryptedMessage));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
